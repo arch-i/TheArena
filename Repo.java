a261e31aa67e1945664373ab168b35c22ce39fbf
arch-i <Archi.gupta2@gmail.com>
2017-05-18 02:02:21 +0530

    Update Repo.java

--- Repo.java
+++ Repo.java
@@ -5,27 +5,27 @@ import java.net.URL;
 
 import com.google.gson.Gson;
 
-public class Repo {
+public class Repo {//representation of json repo list api
 	String full_name, language;
 	boolean fork;
 	int stargazers_count, forks_count;
 	
-	public static Repo[] getRepos(String login){
+	public static Repo[] getRepos(String login){//Creates Repo list (array) using login(user-id)
 		try{
-			URL url = new URL("https://api.github.com/users/"+login+"/repos");
+		    URL url = new URL("https://api.github.com/users/"+login+"/repos");//requesting repo list api
 		    HttpURLConnection con = (HttpURLConnection)url.openConnection();
-		    InputStream is = con.getInputStream();
+		    InputStream is = con.getInputStream();//json format
 		    Gson gson = new Gson();
-		    Repo[] repo = gson.fromJson(new InputStreamReader(is), Repo[].class);
+		    Repo[] repo = gson.fromJson(new InputStreamReader(is), Repo[].class);//Creates an array of Repos from json
 		    return repo;
 		}catch(Exception e){System.out.println("No Repos Found");}
 		return new Repo[0];
 	}
-	public int getStars(){
+	public int getStars(){//returns Stars on the repo
 		return stargazers_count;
 	}
 	
-	public int getForks(){
+	public int getForks(){//returns forks on the repo
 		return forks_count;
 	}
 
