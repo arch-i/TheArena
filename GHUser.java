8216284ec498437eed060fd4ed777968cd637b11
arch-i <Archi.gupta2@gmail.com>
2017-05-18 00:12:39 +0530

    Update GHUser.java

--- GHUser.java
+++ GHUser.java
@@ -2,12 +2,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
 import com.google.gson.Gson;
 
-public class GHUser {
-	protected String login;//Represents a GitHub User (object) definied by field login (user-id)
+public class GHUser {//Represents a GitHub User (object) definied by field login (user-id)
+	protected String login;
 	
-    public static GHUser getUser(String token){// Returns user with the given token
+    public static GHUser getUser(String token){// Creates(returns) user with the given token
 		try{
 			URL url = new URL("https://api.github.com/user?access_token="+token);//making request to the user profile API  
 			HttpURLConnection con = (HttpURLConnection)url.openConnection();
