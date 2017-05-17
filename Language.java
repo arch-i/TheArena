6c65f0f1b43b4cf958ff58b6359de71b9cd7087a
arch-i <Archi.gupta2@gmail.com>
2017-05-18 01:46:38 +0530

    Update Language.java

--- Language.java
+++ Language.java
@@ -1,44 +1,46 @@
 
-public class Language {
+public class Language {//keeps the record of number of times (count) a particular language has been used
 	String lang;
 	int count;
-	public static String getLanguage(Repo[] repo){
-		Language[] l= new Language[100];
+	public static String getLanguage(Repo[] repo){// returns the most used programming language in the repo list
+		Language[] l= new Language[100];//an array of null languages 
 		for(int j=0;j<100;j++){
 			l[j] = new Language();
 			l[j].lang=null;
 			l[j].count=0;
 		}
-		String mostl=null ;
-		int max=0;
+		
 		for(int i=0;i<(repo.length);i++){
-			String nextl = repo[i].language;
-			System.out.println(nextl);
+			String nextL = repo[i].language;// nextL = language of ith repo
 			int j=0;
-			while(nextl!=(l[j].lang)&&l[j].lang!=null){
+			while(nextL!=(l[j].lang)&&l[j].lang!=null){//checks wether nextL matches with any previous Ls(l[j] languages)
 				j++;
 			}
-			if(nextl!=null&&l[j].lang==null){
-				l[j].lang = nextl;
+			if(nextL!=null&&l[j].lang==null){// if no match found then nextL is alloted to jth Language
+				l[j].lang = nextL;
 				l[j].count = 1;
 			}
-			if(nextl==(l[j].lang)&&l[j].lang!=null){
+			if(nextL==(l[j].lang)&&l[j].lang!=null){// if match found then l[j]'s count is increased
 				l[j].count++;
 			}
 		}
-		if(l[0].count==0){
+		
+		if(l[0].count==0){// no language found in any repo
 			return null;
 		}
+		
+		String mostL=null ;//= most used language
+		int max=0;
 		for(int i=0;i<l.length;i++){
 			if(l[i].count>max){
 				max = l[i].count;
-				mostl = l[i].lang;
+				mostL = l[i].lang;
 			}
 			if(l[i].count==max){
-				mostl = mostl+" and "+l[i].lang;
+				mostL = mostL+" and "+l[i].lang;
 			}
 		}
-		return mostl;
+		return mostL;
 	}
 
 }
