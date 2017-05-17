7978bf5a2ef82e9f83ed75d8c719461791d6b1c0
arch-i <Archi.gupta2@gmail.com>
2017-05-18 02:20:21 +0530

    Update Arena.java

--- Arena.java
+++ Arena.java
@@ -1,14 +1,14 @@
 
 public class Arena {
 	public static void main(String args[]){
-		String token1 = args[0], token2 = args[1];
-		GHUser user1 = GHUser.getUser(token1);
+		String token1 = args[0], token2 = args[1];//storing inputs
+		GHUser user1 = GHUser.getUser(token1);//creating users from tokens
 		GHUser user2 = GHUser.getUser(token2);
-		String login1 = user1.login;
+		String login1 = user1.login;//getting their user-id
 		String login2 = user2.login;
-		Repo[] repo1 = Repo.getRepos(login1);
+		Repo[] repo1 = Repo.getRepos(login1);//getting the list of repos of the user
 		Repo[] repo2 = Repo.getRepos(login2);
-		new PrintUser(repo1,"A");
+		new PrintUser(repo1,"A");//printing stuff for the user
 		new PrintUser(repo2,"B");
 	}
 
