9cff6219bf793c65b4f4f7551488fb7454a15625
arch-i <Archi.gupta2@gmail.com>
2017-05-18 21:36:46 +0530

    Update PrintUser.java

--- PrintUser.java
+++ PrintUser.java
@@ -5,7 +5,7 @@ public class PrintUser {
 		int totalC=0, lastWeek=0;// totalC = total commits of a user //lastWeek = last week's repo
 		System.out.println("User "+name+":\n\t Total Repos: "+repo.length);//prints total owned repos
 		for(int i=0;i<repo.length;i++){//prints details of each repo
-			System.out.println("\t\t Repo "+(i+1)+": Stars - "+repo[i].getStars()+", Forks - "+repo[i].getForks());// prints Stars, Forks
+			System.out.print("\t\t Repo "+(i+1)+": Stars - "+repo[i].getStars()+", Forks - "+repo[i].getForks());// prints Stars, Forks
 			Commits commits = Commits.getCommits(repo[i]);
 			int repoC = commits.getTotal();//total commits of a repo
 			totalC = totalC + repoC;//totaling commits for all repos
@@ -13,6 +13,7 @@ public class PrintUser {
 			if(repo[i].fork==true){//if forked
 				System.out.print(", Commits - "+repoC);//prints commits done in that repo
 			}
+			System.out.print("\n");
 		}
 		System.out.println("\t Total Commits: "+totalC);//prints total commits by the user (in its repos)
 		System.out.println("\t Last Week's Commits: "+lastWeek);//prints total last week's commits (in its repos)
