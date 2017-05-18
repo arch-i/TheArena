4dc54a8e110d607407c93fbe15e961eb16051778
arch-i <Archi.gupta2@gmail.com>
2017-05-18 21:16:49 +0530

    Update Arena.java

--- Arena.java
+++ Arena.java
@@ -1,7 +1,7 @@
 
 public class Arena {
 	public static void main(String args[]){
-		String token1 = args[0], token2 = args[1];//storing inputs
+		try{String token1 = args[0], token2 = args[1];//storing inputs
 		GHUser user1 = GHUser.getUser(token1);//creating users from tokens
 		GHUser user2 = GHUser.getUser(token2);
 		String login1 = user1.login;//getting their user-id
@@ -10,6 +10,7 @@ public class Arena {
 		Repo[] repo2 = Repo.getRepos(login2);
 		new PrintUser(repo1,"A");//printing stuff for the user
 		new PrintUser(repo2,"B");
+		   }catch(ArrayIndexOutOfBoundsException e){System.out.println("please enter the access tokens");}
 	}
 
 }
